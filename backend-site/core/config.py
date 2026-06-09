bf414d1d050d081e19ec892de0cb05aa0a47e2ce
Nakatorika <sopanha.sin.passerellesnumeriques.org>
2026-06-09 10:02:10 +0700

    update entry point

--- backend-site/core/config.py
+++ backend-site/core/config.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
 
     PROJECT_NAME: str = "AI Weather Care"
     VERSION: str = "1.0.0"
-    API_V1_STR: str = "/api/v1"
+    API_V1_STR: str = "/v1"
 
     BACKEND_CORS_ORIGINS: List[str] = ["*"]
     # Google Forms
