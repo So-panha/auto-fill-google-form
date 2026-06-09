8b45b2b8f213d4647b3f27c98b7ef4ca03af3e6b
Nakatorika <sopanha.sin.passerellesnumeriques.org>
2026-06-09 09:36:51 +0700

    update vue

--- frontend-site/app/src/components/AutoFillForm.vue
+++ frontend-site/app/src/components/AutoFillForm.vue
@@ -172,7 +172,7 @@ const initiateAutomatedFlow = async () => {
   payload.append('file', fileState.rawFile);
 
   try {
-    const response = await fetch('${import.meta.env.VITE_API_BASE_URL}/google_form/auto_fill', {
+    const response = await fetch('${import.meta.env.VITE_API_BASE_URL}/api/v1/google_form/auto_fill', {
       method: 'POST',
       body: payload
     });
